3939bcc12ac4c5a3b86dd83aabaa552d4f1a77ca
wzx <940945444@qq.com>
2020-08-06 00:25:25 +0800

    修改eureka服务端，能单独启动

--- eureka/src/main/java/com/course/EurekaApplication.java
+++ eureka/src/main/java/com/course/EurekaApplication.java
@@ -2,8 +2,16 @@ package com.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+* eureka  服务端
+* @author Winston.Wang
+* @date 2020/8/5
+* @version 1.0
+**/
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
 	public static void main(String[] args) {
