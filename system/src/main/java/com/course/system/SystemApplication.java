27894c614d6ea374543d68c006d03022e7eb3d6f
wzx <940945444@qq.com>
2020-08-06 23:49:17 +0800

    将system工程修改为eureka client 工程

--- system/src/main/java/com/course/system/SystemApplication.java
+++ system/src/main/java/com/course/system/SystemApplication.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.core.env.ConfigurableEnvironment;
 * @version 1.0
 **/
 @SpringBootApplication
+@EnableEurekaClient
 public class SystemApplication {
     private final static Logger logger = LoggerFactory.getLogger(SystemApplication.class);
     public static void main(String[] args) {
