c9f11d7a910e2f09f342b8c1fdac770fc7bac1e6
wzx <940945444@qq.com>
2020-08-09 13:17:00 +0800

    增加springboot 热部署插件

--- doc/sql/all.sql
+++ doc/sql/all.sql
@@ -1,8 +1,9 @@
-drop  table  if exists test;
+DROP  TABLE  IF EXISTS TEST;
 CREATE TABLE `test` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
-alter  table  test comment '测试表';
+ALTER  TABLE  TEST COMMENT '测试表';
+INSERT INTO course.test (name) VALUES ('测试1');
